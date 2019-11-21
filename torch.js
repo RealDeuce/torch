7033efc42bdf338d1bea78e8393df61a257d1050
Stephen Hurd <shurd@sasktel.net>
2019-11-21 00:57:44 -0500

    Add Beerware license.

--- torch.js
+++ torch.js
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <shurd@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.        Stephen Hurd
+ * ----------------------------------------------------------------------------
+ */
+
 class Torch {
 	static addTorchButton(app, html, data) {
 
